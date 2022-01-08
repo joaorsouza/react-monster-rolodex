3921e3da16e0f53ab378bb21398605ce7b9a9c6f
João Rafael de Souza <joaorsouza@yahoo.com.br>
2022-01-08 10:03:27 -0300

    Add monsters array validation

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import './card-list.css'
 
 export const CardList = (props) =>{
-    console.log(props)
-    return <div className='card-list'>{props.state.monsters.map((monster) => (
-        <h1 key={monster.id}>{monster.name}</h1>
-      ))}
+    return<div className='card-list'>
+     {props.monsters?.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
     </div>
 
 }
